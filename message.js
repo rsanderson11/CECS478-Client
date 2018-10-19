d4e1dfbaf0e4cb67545ce168434b24c530bddb9a
Robert Sanderson <robertasanderson11@gmail.com>
2018-10-18 21:26:08 -0700

    Added encryption module.

--- message.js
+++ message.js
@@ -1,57 +1,81 @@
+const fs = require('fs');
+const NodeRSA = require('node-rsa');
+const crypto = require('crypto');
+const utf8 = require('utf8');
+
+const pkfilepath = 'public.pem';
+
 // Outputs user input from text box to another area on the page.
 function send(){
   // Get the value of the element with Id 'input'
-  var textInput = document.getElementById("input").value;
+  var messageString = document.getElementById('input').value;
 
   // Print our message to the element with Id 'print-output'
-  document.getElementById("print-output").innerHTML = textInput;
+  document.getElementById('print-output').innerHTML = messageString;
+
+  // Encrypt message
+  encrypt(messageString, pkfilepath);
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // TODO: Encrypter
+  // Input is a message string and an RSA public key file path (.pem file).
+  function encrypt(message, pkp){
+
+    // Generate RSA object
+    const encryptRSA = new NodeRSA();
+    // Read file and get contents
+    var publickeydata = fs.readFileSync(pkfilepath);
+    // Load public key
+    encryptRSA.importKey(publickeydata);
+
+
+    // Generate 256-bit key for AES
+    var AESkey = crypto.randomBytes(32);
+    //console.log(AESkey.toString('hex'));
+
+    // Generate a 16-bit Initialization Vector
+    var iv = crypto.randomBytes(16);
+    // Initialize an AES object.
+    var AEScipher = crypto.createCipheriv('aes-256-cbc', AESkey, iv);
+
+    // Encypt message with AES
+    var AES_ciphertext = AEScipher.update(message, 'utf8', 'hex');
+    AES_ciphertext += AEScipher.final('hex');
+    AES_ciphertext = iv.toString('hex') + AES_ciphertext;
+
+
+    // Generate 256-bit key for HMAC
+    var HMACkey = crypto.randomBytes(32);
+        //console.log(HMACkey.toString('hex'));
+    // Initialize HMAC object
+    var hmac = crypto.createHmac('sha256', HMACkey);
+    // Compute integrity tag by encrypting ciphertext with our HMAC object
+    var integrityTag = hmac.update(AES_ciphertext);
+        //console.log(integrityTag);
+    integrityTag = hmac.digest('hex');
+        //console.log(integrityTag);
+
+    // Concatenate AES and HMAC keys
+    var concatKeys = AESkey.toString('hex') + HMACkey.toString('hex');
+        //console.log(concatKeys);
+
+    // Encrypt concatenated keys with RSA object
+    var encryptedKeys = encryptRSA.encrypt(concatKeys);
+        //console.log(encryptedKeys.toString('hex'));
+
+    console.log("RSA cipher text: " + encryptedKeys.toString('hex'));
+    console.log("AES cipher text: " + AES_ciphertext);
+    console.log("HMAC tag: " + integrityTag);
+  }
 }
-
-// TODO: Encrypter
-// Input is a message string and an RSA public key file path (.pem file).
-
-// Initialize an RSA object (OAEP only).
-// RSA object will load the public key.
-// Initialize an AES object and generate a 256-bit AES key.
-// Encrypt message with AES.
-// Generate an HMAC 256-bit key.
-// Run HMAC (SHA 256) on ciphertext to compute the inegrity tag.
-// Concatenate the keys (AES and HMAC) and encrypt the keys with the RSA object.
-// Output the RSA ciphertext, AES ciphertext and HMAC tag (JSON).
-
-
-// Example of modular design
-var Module = (function () {
-
-  var privateMethod = function () {
-    // private
-  };
-
-  var someMethod = function () {
-    // public
-  };
-
-  var anotherMethod = function () {
-    // public
-  };
-
-  return {
-    someMethod: someMethod,
-    anotherMethod: anotherMethod
-  };
-
-})();
-// End example
-
-
-// TODO: Decrypter
-// Inputs: JSON object with keys as: RSA Ciphertext, AES ciphertext, HMAC tag, a file path to an RSA private keys
-
-// Initialize an RSA object (OAEP only; 2048 bits key size).
-// Load the private key into your RSA private key.
-// Decrypt the RSA Ciphertext (from JSON) and recover a pair of 256-bit keys (one is AES, other is HMAC).
-// Run HMAC (SHA 256 with HMAC key^) to regenerate tag.
-// Compare this tag with input tag (from JSON).
-// If no match, return failure.
-// Otherwise, continue by Initializing an AES key (same as encrypter module) and decrypt the AES ciphertext (from JSON input).
-// Return recovered plaintext (or failure at any step).
